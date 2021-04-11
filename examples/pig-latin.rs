97a67f5d183ba06343c47a85495bdcde9bf24e10
Bart Massey <bart.massey@gmail.com>
2021-04-10 18:15:20 -0700

    replaced word matcher for pig-latin with regex version

--- examples/pig-latin.rs
+++ examples/pig-latin.rs
@@ -16,6 +16,9 @@
 
 use is_vowel::IsRomanceVowel;
 
+use once_cell::sync::Lazy;
+use regex::Regex;
+
 /// Transform `word` to [Pig Latin](https://en.wikipedia.org/wiki/Pig_Latin). Word is assumed to
 /// be in a [Romance language](https://en.wikipedia.org/wiki/Romance_languages): see
 /// `[IsRomanceVowel][IsRomanceVowel]::[is_romance_vowel][is_romance_vowel]` for the definition
@@ -24,18 +27,14 @@ use is_vowel::IsRomanceVowel;
 /// `vowel_suffix_start` is the string appended before "ay" after words beginning with a vowel:
 /// this is usually "w" or "h", but also might be "j", "" or any of a number of other things
 /// depending on the Pig Latin "dialect".
-pub fn word_to_pig_latin<S, T>(word: S, vowel_suffix_start: T) -> String
-    where S: AsRef<str>, T: AsRef<str>
-{
-    let rword = word.as_ref();
-    let mut chars = rword.chars();
+pub fn word_to_pig_latin(word: &str, vowel_suffix_start: &str) -> String {
+    let mut chars = word.chars();
     let first = chars.next();
     let mut result = match first {
         Some(first) => {
             if first.is_romance_vowel() {
-                let suf = vowel_suffix_start.as_ref();
-                let mut result = rword.to_string();
-                result += suf;
+                let mut result = word.to_string();
+                result += vowel_suffix_start;
                 result
             } else {
                 let mut result: String = chars.collect();
@@ -44,7 +43,7 @@ pub fn word_to_pig_latin<S, T>(word: S, vowel_suffix_start: T) -> String
             }
         }
         None => {
-            return rword.to_string();
+            return word.to_string();
         }
     };
     result += "ay";
@@ -52,36 +51,31 @@ pub fn word_to_pig_latin<S, T>(word: S, vowel_suffix_start: T) -> String
 }
 
 /// Map alphabetic words in `text` using the `word_processor` mapping function.
-fn map_words<S, F>(text: S, mut word_processor: F) -> String
-    where S: AsRef<str>, F: FnMut(&str) -> String
+fn map_words<F>(text: &str, mut word_processor: F) -> String
+    where F: FnMut(&str) -> String
 {
-    let text: Vec<char> = text.as_ref().chars().collect();
-    let mut start: &[char] = &text;
-    let mut result = String::with_capacity(start.len());
-    while !start.is_empty() {
-        if start[0].is_alphabetic() {
-            let index = match start.find(|c| !c.is_alphabetic()) {
-                Some(i) => i,
-                None => start.len(),
-            };
-            let word: String = start[..index].iter().cloned().collect();
-            result += &word_processor(&word);
-            start = &start[index..];
-        } else {
-            result.push(start[0]);
-            start = &start[1..];
-        }
-    }
-    result
+    static WORD_RE: Lazy<Regex> = Lazy::new(|| Regex::new(
+        r"(?x)
+            \p{Alphabetic} (
+                ( \p{Alphabetic}
+                  | \p{Join_Control}
+                  | \p{Mark}
+                  | \p{Connector_Punctuation}
+                  | ['’]
+                )*
+              \p{Alphabetic} )?
+        "
+    ).unwrap());
+    WORD_RE.replace_all(text, |w: &regex::Captures| {
+        word_processor(w.get(0).unwrap().as_str())
+    }).to_string()
 }
 
 /// Transform `text` to [Pig Latin](https://en.wikipedia.org/wiki/Pig_Latin).
 /// See [`to_pig_latin_word`][to_pig_latin_word] for an explanation of
 /// `vowel_suffix_start`.
-pub fn text_to_pig_latin<S, T>(text: S, vowel_suffix_start: T) -> String
-    where S: AsRef<str>, T: AsRef<str>
-{
-    map_words(text.as_ref(), |w| word_to_pig_latin(w, vowel_suffix_start.as_ref()))
+pub fn text_to_pig_latin(text: &str, vowel_suffix_start: &str) -> String {
+    map_words(text.as_ref(), |w| word_to_pig_latin(w, vowel_suffix_start))
 }
 
 /// Emit command-line argument text as Pig Latin with "h" vowel suffix.
@@ -90,5 +84,5 @@ pub fn main() {
         .skip(1)
         .collect::<Vec<_>>()
         .join(" ");
-    println!("{}", text_to_pig_latin(text, "h"));
+    println!("{}", text_to_pig_latin(&text, "h"));
 }
