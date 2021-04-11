9e3de7e6b3f26e10dcfd515dec4f10fe0673501d
Bart Massey <bart.massey@gmail.com>
2021-04-10 20:13:25 -0700

    added license

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 //! Text to [Pig Latin](https://en.wikipedia.org/wiki/Pig_Latin) conversion.
 //!
-//! Began as a solution to [Exercise
-//! 8.2](https://doc.rust-lang.org/book/ch08-03-hash-maps.html#summary) in [The Rust
-//! Programming Language](https://doc.rust-lang.org/book/) The innocuous-looking phrase "Keep
-//! in mind the details about UTF-8 encoding!" in that exercise conceals a world of pain.
+//! This crate began as a solution to [Exercise
+//! 8.2](https://doc.rust-lang.org/book/ch08-03-hash-maps.html#summary) in [The Rust Programming
+//! Language](https://doc.rust-lang.org/book/) The innocuous-looking phrase "Keep in mind the
+//! details about UTF-8 encoding!" in that exercise conceals a world of pain.
 //!
 //! At the end of the day, Pig Latin is not a well-defined dialect. This program will produce
 //! questionable results for complicated cases and non-Romance languages (for which the
@@ -12,13 +12,10 @@
 //!
 //! # Authors
 //!
-//! * Reddit
-//!   [/u/AlexRodger](https://reddit.com/u/AlexRodger) wrote
-//!   the [original
+//! * Reddit [/u/AlexRodger](https://reddit.com/u/AlexRodger) wrote the [original
 //!   code](https://www.reddit.com/r/learnrust/comments/mo5lvd/rate_and_critic_my_solution_to_exercise_2_in/).
 //!
-//! * Reddit [/u/hjd_thd](https://reddit.com/u/hjd_thd)
-//!   rewrote to get an [FP
+//! * Reddit [/u/hjd_thd](https://reddit.com/u/hjd_thd) rewrote to get an [FP
 //!   solution](https://www.reddit.com/r/learnrust/comments/mo5lvd/rate_and_critic_my_solution_to_exercise_2_in/gu1w5s6).
 //!
 //! * Reddit [/u/po8](https://reddit.com/u/po8) rewrote again into a more production-grade
